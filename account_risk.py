da679fb64abf1cd0ccd466c88c650c4328c12511
Joe Roy <127060760+JoeRoy1998@users.noreply.github.com>
2026-02-02 21:58:53 -0500

    Create account_risk.py

---
+++ account_risk.py
@@ -0,0 +1,88 @@
+def assess_account_risk(username, user_data):
+    # analyze user_data here
+    failed_logins = user_data["failed_logins"]
+    role = user_data["role"]
+    account_locked = user_data["account_locked"]
+
+    if role == "Executive":
+        if failed_logins >= 5:
+            severity = "LOCKED"
+            account_locked = True
+        else:
+            severity = "OK"
+            account_locked = False
+    elif role == "IT":
+        if failed_logins >= 10:
+            severity = "LOCKED"
+            account_locked = True
+        elif failed_logins >= 5:
+            severity = "ALERT"
+            account_locked = False
+        else:
+            severity = "OK"
+            account_locked = False
+    elif role == "User":
+        if failed_logins >= 15:
+            severity = "LOCKED"
+            account_locked = True
+        elif failed_logins >= 10:
+            severity = "ALERT"
+            account_locked = False
+        elif failed_logins >= 5:
+            severity = "CRITICAL"
+            account_locked = False
+        else:
+            severity = "OK"
+            account_locked = False
+
+    # return a message
+    if account_locked == True:
+        return f"[{severity}]: {username} ({role}) account is locked. Reach out to IT for further assistance."
+    else:
+        if role == "Executive":
+            return f"[{severity}] {username} ({role}) is exhibiting normal behavior."
+        elif role == "IT":
+            if severity == "ALERT":
+                return f"[{severity}] {username} ({role}) has failed to login several times."
+            else:
+                return f"[{severity}] {username} ({role}) is exhibiting normal behavior."
+        elif role == "User":
+            if severity == "ALERT":
+                return f"[{severity}] {username} ({role}) is seeing a large amount of failed logins."
+            elif severity == "CRITICAL":
+                return f"[{severity}] {username} ({role}) is exhibiting abnormal login behavior!"
+            else:
+                return f"[{severity}] {username} ({role}) is exhibiting normal behavior."
+
+
+accounts = {
+    "admin": {
+        "failed_logins": 2,
+        "role": "IT",
+        "account_locked": False
+    },
+    "exec": {
+        "failed_logins": 12,
+        "role": "Executive",
+        "account_locked": True
+    },
+    "jdoe": {
+        "failed_logins": 6,
+        "role": "User",
+        "account_locked": False
+    },
+    "jroy": {
+        "failed_logins": 20,
+        "role": "IT",
+        "account_locked": True
+    },
+    "dseale": {
+        "failed_logins": 6,
+        "role": "IT",
+        "account_locked": True
+    }
+}
+
+for user, data in accounts.items():
+    # call your function and print result
+    print(assess_account_risk(user, data))
